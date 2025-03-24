bc365a4978420206506955cd4476143f555350aa
tpavelv <tpavelv@icloud.com>
2025-03-24 17:53:59 +0800

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import List from '../children/Children'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <List>
+        123
+        <li>321</li>
+      </List>
+    </div>
+  )
+}
+
+export default App
